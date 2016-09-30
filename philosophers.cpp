afa43d01d1ccc963ac90d557e965de173941972c
Pavel S. Smirnov <paul.smirnov.aka.sps@gmail.com>
2016-09-30 19:21:16 +0300

    refactoring: extract class Monitor, Canteen now template<Monitor> Canteen

--- philosophers.cpp
+++ philosophers.cpp
@@ -76,11 +76,10 @@ private:
 template<typename Tag, typename id_type>
 ID_generator<id_type> Object_with_id<Tag, id_type>::m_generator;
 
-
 /// Fork is simple mutex
 typedef std::mutex Fork;
 
-class Canteen;
+class Monitor;
 
 class Philosopher
     : public Object_with_id<Philosopher>
@@ -92,12 +91,13 @@ public:
         hungry,
         dines
     };
-    Philosopher(Canteen* _p_canteen, Fork* _p_left, Fork* _p_right)
+    Philosopher(Monitor* _p_canteen, std::shared_ptr<Fork> const& _p_left, std::shared_ptr<Fork> const& _p_right)
         : m_state(States::thinks)
-        , m_p_canteen(_p_canteen)
+        , m_p_monitor(_p_canteen)
         , m_p_left(_p_left)
         , m_p_right(_p_right)
-    {}
+    {
+    }
 
     States state()const
     {
@@ -123,68 +123,118 @@ public:
             m_p_right->unlock();
         }
     }
-    static void worker(Philosopher* p_philosopfer)
+    /// common thread worker
+    static void worker(std::shared_ptr<Philosopher> const& _p_philosopfer)
     {
-        (*p_philosopfer)();
+        (*_p_philosopfer)();
     }
+
 private:
-    static std::chrono::milliseconds
-        random_interval()
+    static std::chrono::milliseconds random_interval()
     {
         static std::uniform_int_distribution<unsigned> distrigution(1, 10000);
         auto const rnd = distrigution(g_rnd);
         return std::chrono::milliseconds(rnd);
     }
-    void state(States _state);
+    inline void state(States _state);
 
     std::atomic<States> m_state;
     static std::mutex m_mutex;
     static std::condition_variable m_cv;
-    Canteen* m_p_canteen;
-    Fork* m_p_left;
-    Fork* m_p_right;
+    Monitor* m_p_monitor;
+    std::shared_ptr<Fork> m_p_left;
+    std::shared_ptr<Fork> m_p_right;
 };
+
 std::mutex Philosopher::m_mutex;
 std::condition_variable Philosopher::m_cv;
 
-/// Canteen with simple text log
-class Canteen
+class Monitor
 {
-    typedef std::pair<unsigned, Philosopher::States> state_log_element_type;
 public:
-    explicit Canteen(unsigned _number_of_philosophers = 5)
-        : m_number_of_philosophers(_number_of_philosophers)
-    {}
+    typedef std::pair<unsigned, Philosopher::States> state_log_element_type;
     void log_state(Philosopher const* _p_philosopher)
     {
-        std::unique_lock<decltype(m_log_queue_mutex)> locker(m_log_queue_mutex);
-        m_log_queue.emplace_back(_p_philosopher->id(), _p_philosopher->state());
-        m_cv.notify_one();
-    }
-    unsigned number_of_philosophers() const
-    {
-        return m_number_of_philosophers;
-    }
-    void operator()()
-    {
-        std::vector<std::unique_ptr<Fork> > forks;
-        std::generate_n(std::back_inserter(forks), number_of_philosophers(), []() {return std::make_unique<Fork>(); });
-        std::vector<std::unique_ptr<Philosopher> > philosophers;
-        for (unsigned i = 0; i < number_of_philosophers(); ++i) {
-            Fork* const p_left = forks[i].get();
-            Fork* const p_right = forks[(i + 1) % number_of_philosophers()].get();
-            philosophers.push_back(std::make_unique<Philosopher>(this, p_left, p_right));
+        if (_p_philosopher) {
+            std::unique_lock<decltype(m_log_queue_mutex)> locker(m_log_queue_mutex);
+            m_log_queue.emplace_back(_p_philosopher->id(), _p_philosopher->state());
+            m_cv.notify_one();
         }
-
-        std::vector<std::thread> threads;
-        threads.reserve(number_of_philosophers());
-        std::transform(philosophers.cbegin(), philosophers.cend(), std::back_inserter(threads),
-                       [](decltype(philosophers)::value_type const& ptr) {return std::thread(Philosopher::worker, ptr.get()); });
-        log_worker();
+    }
+    void monitor_worker()
+    {
+        decltype(m_log_queue) work_log;
+        for (;;) {
+            {
+                std::unique_lock<decltype(m_log_queue_mutex)> locker(m_log_queue_mutex);
+                while (m_log_queue.empty()) {
+                    m_cv.wait(locker);
+                }
+                std::swap(work_log, m_log_queue);
+            }
+            monitor(work_log);
+            work_log.clear();
+        }
     }
 
 protected:
     typedef std::vector<state_log_element_type> log_queue_type;
+    virtual void monitor(log_queue_type const& work_log) = 0;
+
+    std::mutex m_log_queue_mutex;
+    std::condition_variable m_cv;
+    log_queue_type m_log_queue;
+};
+
+void Philosopher::state(States _state)
+{
+    std::unique_lock<decltype(m_mutex)> locker(m_mutex);
+    m_state = _state;
+    if (m_p_monitor) {
+        m_p_monitor->log_state(this);
+    }
+    m_cv.notify_all();
+}
+
+template<class TyMonitor>
+class Canteen
+{
+public:
+    explicit Canteen(unsigned _number_of_philosophers = 5)
+    {
+        if (_number_of_philosophers < 2) {
+            throw std::invalid_argument("Invalid number of philosophers (<2)");
+        }
+        m_forks.reserve(_number_of_philosophers);
+        std::generate_n(std::back_inserter(m_forks), _number_of_philosophers, []() {return std::make_shared<Fork>(); });
+        
+        m_philosophers.reserve(_number_of_philosophers);
+        for (unsigned i = 0; i < _number_of_philosophers; ++i) {
+            m_philosophers.push_back(std::make_shared<Philosopher>(
+                &m_monitor,
+                m_forks[i],
+                m_forks[(i + 1) % _number_of_philosophers]));
+        }
+    }
+    void operator()()
+    {
+        std::vector<std::thread> threads;
+        threads.reserve(m_philosophers.size());
+        std::transform(m_philosophers.cbegin(), m_philosophers.cend(), std::back_inserter(threads),
+                       [](std::shared_ptr<Philosopher> const& ptr) {return std::thread(Philosopher::worker, ptr); });
+        m_monitor.monitor_worker();
+    }
+
+private:
+    std::vector<std::shared_ptr<Fork> > m_forks;
+    std::vector<std::shared_ptr<Philosopher> > m_philosophers;
+    TyMonitor m_monitor;
+};
+
+class Simple_log_monitor
+    : public Monitor
+{
+protected:
     virtual void monitor(log_queue_type const& work_log)
     {
         for (auto el : work_log) {
@@ -206,42 +256,18 @@ protected:
             std::cout << std::endl;
         }
     }
-
-private:
-    void log_worker()
-    {
-        decltype(m_log_queue) work_log;
-        for (;;) {
-            {
-                std::unique_lock<decltype(m_log_queue_mutex)> locker(m_log_queue_mutex);
-                while (m_log_queue.empty()) {
-                    m_cv.wait(locker);
-                }
-                std::swap(work_log, m_log_queue);
-            }
-            monitor(work_log);
-            work_log.clear();
-        }
-    }
-    
-    unsigned m_number_of_philosophers;
-    std::mutex m_log_queue_mutex;
-    std::condition_variable m_cv;
-    log_queue_type m_log_queue;
 };
 
-/// Canteen with waterfall states log
-class Canteen_waterfall
-    : public Canteen
+class Waterfall_monitor
+    : public Monitor
 {
-public:
-    Canteen_waterfall(unsigned _number_of_philosophers = 5)
-        : Canteen(_number_of_philosophers)
-        , m_buffer(_number_of_philosophers, symb(Philosopher::States::thinks))
-    {}
+protected:
     virtual void monitor(log_queue_type const& work_log)override
     {
         for (auto el : work_log) {
+            if (m_buffer.size() <= el.first) {
+                m_buffer.append(el.first + 1 - m_buffer.size(), symb(Philosopher::States::thinks));
+            }
             m_buffer[el.first] = symb(el.second);
         }
         std::cout << m_buffer << std::endl;
@@ -254,7 +280,7 @@ private:
             return ' ';
             break;
         case Philosopher::States::hungry:
-            return '*';
+            return '-';
             break;
         case Philosopher::States::dines:
             return '|';
@@ -265,22 +291,17 @@ private:
         }
     }
     std::string m_buffer;
+
 };
 
-void Philosopher::state(States _state)
-{
-    std::unique_lock<decltype(m_mutex)> locker(m_mutex);
-    m_state = _state;
-    m_p_canteen->log_state(this);
-    m_cv.notify_all();
-}
 }  // namespace philosophers
 
 int main(int argc, char* argv[])
 {
     try {
-        unsigned const num_ph = argc < 2 ? 5 : atoi(argv[1]);
-        philosophers::Canteen_waterfall canteen(num_ph);
+        unsigned const num_ph = argc < 2 ? 64 : atoi(argv[1]);
+        using namespace philosophers;
+        Canteen<Waterfall_monitor> canteen(num_ph);
         canteen();
         return 0;
     } catch (std::exception const& exc) {
