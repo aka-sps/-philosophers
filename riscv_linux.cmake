cfd017c7308ecd2bc532723c0fd0bf7411e07db5
Pavel S. Smirnov <paul.smirnov.aka.sps@gmail.com>
2016-11-01 19:58:06 +0300

    FIX: working riscv_linux.cmake

--- riscv_linux.cmake
+++ riscv_linux.cmake
@@ -8,17 +8,9 @@
 # An example for using the gumstix arm-linux toolchain is given below.
 # Uncomment to try it out.
 #
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_FIND_ROOT_PATH /opt/riscv32)
 set(TARGET_PREFIX riscv32-unknown-linux-gnu-)
-set(CMAKE_SYSTEM_PROCESSOR riscv32)
-set(CMAKE_C_COMPILER /opt/riscv32/bin/${TARGET_PREFIX}gcc)
-set(CMAKE_CXX_COMPILER /opt/riscv32/bin/${TARGET_PREFIX}g++)
-# search for programs in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-# set(CMAKE_EXE_LINKER_FLAGS -static CACHE STRING "Flags used by the linker")
-SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)
-SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_C_COMPILER ${TARGET_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TARGET_PREFIX}g++)
+set(CMAKE_FIND_ROOT_PATH /opt/riscv32)
+set(CMAKE_EXE_LINKER_FLAGS "-static -pthread" CACHE STRING "Flags used by the linker")
